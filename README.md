f4227da882734921745d1d48d9704a1064da43a2
Edson5Leonard <edsonleonardorojascabia@gmail.com>
2026-02-20 20:48:18 -0500

    Cambios en Alumnos.sql

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Crud-php
-Trabajo en clases que se realizo un crud con culaquier lenguaje
+Trabajo en clases que se realizo un crud con culaquier lenguaje en este caso se hizo con PHP.
+
+
--- alumnos.sql
+++ alumnos.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE IF EXITS datos;
+CREATE DATABASE IF NOT EXITS datos;
 
 USE datos;
 
 CREATE TABLE `alumno` (
-  `cod_estudiante` int(11) NOT NULL,
+  `cod_estudiante` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `dni` int(11) NOT NULL,
   `nombres` varchar(20) NOT NULL,
   `apellidos` varchar(30) NOT NULL
@@ -12,5 +12,5 @@ CREATE TABLE `alumno` (
 INSERT INTO `alumno` (`cod_estudiante`, `dni`, `nombres`, `apellidos`) VALUES
 (101, 75854549, 'Juan Carlos', 'Herrera Sanches'),
 (103, 75854551, 'Jimena', 'Sandobal Gutierre'),
-(105, 75854553, 'jvh turoriales', 'suscribete y dale lik');
+(105, 75854553, 'juan Pablo', 'Rojas Cabia');
 COMMIT;
